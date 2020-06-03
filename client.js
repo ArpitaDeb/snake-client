a4f382c299b23ef949129240d9255589b9e282a0
ArpitaDeb <arpitadeb1990@gmail.com>
2020-06-03 22:48:50 +0000

    name initials

---
+++ client.js
@@ -0,0 +1,28 @@
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: 'localhost',
+    port: 50541
+  });
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server');
+  });
+  
+  conn.on('connect', () => {
+    conn.write('Name: ARP');
+  });
+ 
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+  conn.on('data', () => {
+    console.log('you ded cuz you idled')
+  });
+  return conn;
+}
+module.exports = {
+  connect
+};
